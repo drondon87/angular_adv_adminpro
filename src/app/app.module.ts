1f016ce4eb172f9be73245e777f68b9be85cab4f
drondon87 <domingorondon87@gmail.com>
2020-11-11 18:06:30 +0100

    fin seccion 3 y 4 diseño y rutas

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NopagefoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PagesModule,
+    AuthModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
